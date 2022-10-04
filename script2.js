2fa02315a8069a0f3138cef5bc619c4fc5c0b5fa
unknown <kingjoudel@icloud.com>
2022-10-04 12:05:05 -0400

    lets see

---
+++ script2.js
@@ -0,0 +1,28 @@
+document.getElementById("#wrong1")
+wrong1.addEventListener("click", wrongAnwser);
+
+document.getElementById("#wrong2")
+wrong2.addEventListener("click", wrongAnwser);
+
+document.getElementById("#wrong3")
+wrong3.addEventListener("click", wrongAnwser);
+
+document.getElementById("#right")
+right.addEventListener("click", rightAnwser);
+
+function wrongAnwser(){
+    window.alert("wrong")
+    if (wrongAnwser = "click" ){
+        window.alert("Try again")
+    }
+}
+
+function rightAnwser(){
+    window.alert("right")
+    if (rightAnwser = "click" ){
+        window.open("http://127.0.0.1:5500/Quiz.github.io/index3.html")
+        window.close("http://127.0.0.1:5500/Quiz.github.io/index.html")
+        
+    }
+
+}
